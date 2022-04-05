a6b64bf88720205ae88eee942e20d63287688ba1
Martin Sapak <xsapak06@vutbr.cz>
2022-04-05 09:16:22 +0200

    Message

---
+++ code.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+working_array = np.arange(0, 21, 1)
+working_array = working_array**3
+working_array[working_array > 10] = 0
+value = working_array.sum()
+
+print(f"Pole: {working_array}, Celková hodnota pole: {value}")
